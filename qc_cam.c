083519fd1aa1b7c36d9c1d28d21022f1cc48e584
denis <pernicious.pickle@gmail.com>
2014-02-01 15:57:21 -0700

    Added register set/get methods

--- qc_cam.c
+++ qc_cam.c
@@ -9,16 +9,22 @@
 #define CAM_VENDOR_ID	0x046D
 #define CAM_PRODUCT_ID	0x0840
 
-#define STV_I2C_WRITE 0x400
-#define STV_I2C_READ 0x1410
+#define STV_I2C_WRITE 	0x400
+#define STV_I2C_READ 	0x1410
 
-#define STV_REG23 0x0423
+#define STV_REG23 		0x0423
 
-#define HDCS_ADDR 0xaa
-#define HDCS_IDENT 0x00
+#define HDCS_ADDR 		0xaa
+#define HDCS_IDENT 		0x00
 
-#define PB_ADDR 0xba
-#define PB_IDENT 0x00
+#define PB_ADDR 		0xba
+#define PB_IDENT 		0x00
+
+#define USB_TIMEOUT		500
+#define USB_REQ			0x04
+#define USB_INDEX		0x0
+#define WRITE_REQ		0x40
+#define READ_REQ		0xC0
 
 unsigned char * data_alloc(int len){
 	unsigned char *data = (unsigned char*)malloc(len);
@@ -61,6 +67,28 @@ struct libusb_device *qc_get_device(libusb_context *context, libusb_device **dev
 	return NULL;
 }
 
+//sets a 2byte register to a value
+void qc_set_reg(libusb_device_handle *handle, int reg, unsigned char val){
+	unsigned char data[2];
+	data[0] = val & 0xFF;
+	data[1] = (val >> 8) & 0xFF;
+	libusb_control_transfer(handle, WRITE_REQ, USB_REQ, reg, USB_INDEX, data, 2, USB_TIMEOUT);
+}
+
+//get a 2byte value from a register
+int qc_read_reg(libusb_device_handle *handle, int reg){
+	unsigned char data[2];
+	data[0] = 0xAA;
+	data[1] = 0xAA;
+	libusb_control_transfer(handle, READ_REQ, USB_REQ, reg, USB_INDEX, data, 2, USB_TIMEOUT);
+	int val = data[0] + (data[1] << 8);
+	return val;
+}
+
+void qc_write_data(libusb_device_handle *handle, unsigned char data[], int dataLen){
+	libusb_control_transfer(handle, WRITE_REQ, USB_REQ, STV_I2C_WRITE, USB_INDEX, data, dataLen, USB_TIMEOUT);
+}
+
 void qc_init(libusb_device_handle *handle){
 	int err = 0;
 	if (libusb_kernel_driver_active(handle, 0))
@@ -91,13 +119,9 @@ void qc_init(libusb_device_handle *handle){
 	unsigned char *data = data_alloc(dataLen);
 	data[0] = 1;
 
-	BytesWritten = libusb_control_transfer(handle, requestType, request, val, index, data, dataLen, nTimeout);
-	//printf( "wrote %d bytes to endpoint address 0x%X\n", BytesWritten, nEndpoint );
+	qc_set_reg(handle, STV_REG23, 1);
 
 	//Request ID (i think)
-	requestType = 0x40;
-	request = 0x04;
-	val = STV_I2C_WRITE;
 	dataLen = 0x23;
 	data = data_alloc(dataLen);
 	data[0x00] = PB_IDENT;
@@ -105,29 +129,19 @@ void qc_init(libusb_device_handle *handle){
 	data[0x21]=0;		// 1 value
 	data[0x22]=3;		// Write cmd
 
-	BytesWritten = libusb_control_transfer(handle, requestType, request, val, index, data, dataLen, nTimeout);
-	//printf( "wrote %d bytes to endpoint address 0x%X\n", BytesWritten, nEndpoint );
+	qc_write_data(handle, data, dataLen);
 
 	//Get sensor ID
-	requestType = 0xc0;
-	request = 0x04;
-	val = STV_I2C_READ;
-	dataLen = 0x02;
-	data = data_alloc(dataLen);
-	data[0] = 0xaa;
-	data[1] = 0xaa;
-
-	BytesWritten = libusb_control_transfer(handle, requestType, request, val, index, data, dataLen, nTimeout);
-	//printf( "read %d bytes to endpoint address 0x%X\n", BytesWritten, nEndpoint );
-
-	if (data[1]==0x64)
+	int chip_type = qc_read_reg(handle, STV_I2C_READ);
+	if (chip_type==0x6403)
 	{
 		printf("This Quickcam has a Photobit PB100 sensor\n");
 	}else{
 		printf("Not sure what camera this is...:(\n");
 	}
 
-	
+	//set all values to defaults
+	//TODO: get all macros from other driver file
 }
 
 int main(int argc, const char* argv[])
